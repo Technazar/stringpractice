3eb887c8a3cf247492c45ebec1e0105e30352e5b
Technazar <technazar2024@gmail.com>
2024-07-11 21:06:26 -0700

    initial commit

---
+++ dowhiledemo.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class dowhiledemo
+{
+public static void main(String[] args) throws IOException
+{
+  int i;
+  i=2;
+   do
+  {
+
+  System.out.println(i);
+   i=i+2;
+}
+ while(i<=20);
+} 
+}
